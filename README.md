75484a5d771b14bf0d56e404614c6028036cb97b
Niklas Rast <47035125+niklasrast@users.noreply.github.com>
2023-07-14 14:42:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Intune Configuration as Code
+
+![GitHub repo size](https://img.shields.io/github/repo-size/niklasrast/Intune-Configuration-as-Code)
+
+![GitHub issues](https://img.shields.io/github/issues-raw/niklasrast/Intune-Configuration-as-Code)
+
+![GitHub last commit](https://img.shields.io/github/last-commit/niklasrast/Intune-Configuration-as-Code)
+
+# +++ THIS REPO IS IN DEVELOPMENT +++
+A documentation will be available later
+
+## Requirements:
+- PowerShell 5.0
+- Azure AD
+- Azure AD App registration
+
+# Feature requests
+If you have an idea for a new feature in this repo, send me an issue with the subject Feature request and write your suggestion in the text. I will then check the feature and implement it if necessary.
+
+Created by @niklasrast 
---
+++ config.ps1
@@ -0,0 +1,51 @@
+<#
+
+.COPYRIGHT
+Copyright (c) Niklas Rast.
+Version 14-07-2023
+
+.DOCUMENTATION
+<<<FOLLOWING SOON>>>
+#>
+
+####################################################
+# Script Parameters
+Param (
+  [Parameter (Mandatory= $true)]
+  [String] $clientId,
+  [Parameter (Mandatory= $true)]
+  [String] $clientSecret,
+  [Parameter (Mandatory= $true)]
+  [String] $tenantId
+)
+
+####################################################
+# Settings
+$ErrorActionPreference = "SilentlyContinue"
+
+####################################################
+# MS Graph Module Install, Update & Connect
+if (Get-Module -ListAvailable -Name Microsoft.Graph) {
+  Import-Module -Name Microsoft.Graph
+  Write-Host "Imported Microsoft.Graph Module" -ForegroundColor Green
+} 
+else {
+  Install-Module -Name Microsoft.Graph
+  Import-Module -Name Microsoft.Graph
+  Write-Host "Installed and Imported Microsoft.Graph Module" -ForegroundColor Green
+}
+
+$latestMSGraphModule = Find-Module -Name Microsoft.Graph -AllVersions -AllowPrerelease | select-Object -First 1
+$currentMSGraphModule = Get-InstalledModule | Where-Object {$_.Name -eq "Microsoft.Graph"}
+
+If ($latestMSGraphModule.version -gt $currentMSGraphModule.version) {
+    try {Update-Module -Name Microsoft.Graph -RequiredVersion $latestMSGraphModule.version -AllowPrerelease
+         Write-host "Microsoft Graph PowerShell module updated successfully to" $latestMSGraphModule.Version -ForegroundColor Green}
+    catch {Write-Host "Unable to update Microsoft Graph PowerShell module" -ForegroundColor Red}
+} else {
+    write-host "Latest version of Microsoft Graph is not newer than the current" -ForegroundColor yellow
+}
+
+$clientSecretPassword = ConvertTo-SecureString -String $clientSecret -AsPlainText -Force
+$ClientSecretCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $clientSecretPassword
+Connect-MgGraph -TenantId $tenantID -ClientSecretCredential $ClientSecretCredential
